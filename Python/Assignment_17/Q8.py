8a462af69bcc601065d10dd7decb8efc508249db
SurajGardi <surajgardi0707@gmail.com>
2026-07-11 01:00:36 +0530

    Assignment_17 Q8 added

---
+++ Python/Assignment_17/Q8.py
@@ -0,0 +1,25 @@
+# 8. Write a program which accept one number and display below pattern.
+# Input : 5
+# Output : 
+#           1
+#           1 2
+#           1 2 3
+#           1 2 3 4
+#           1 2 3 4 5
+
+
+def DisplayPattern(No):
+    
+    for i in range(1, No+1):
+        for j in range(i):
+            print(j+1, end=" ")
+        print()
+
+def main():
+
+    Value = int(input("Enter Number : "))
+    
+    DisplayPattern(Value)
+
+if __name__ == "__main__":
+    main()
