c009bb3ac1a4bf27156377b4b9819efb59116c7a
SurajGardi <surajgardi0707@gmail.com>
2026-07-19 01:51:11 +0530

    Assignment_20 Q_01 added

---
+++ Python/Assignment_20/Q1.py
@@ -0,0 +1,33 @@
+# 1: Design a Python application that creates two separate threads named Even and Odd.
+# • The Even thread should display the first 10 even numbers.
+# • The Odd thread should display the first 10 odd numbers.
+# • Both threads should execute independently using the threading module.
+# • Ensure proper thread creation and execution.
+
+
+import threading
+
+def DisplayEven(No):
+    print(threading.current_thread().name)
+    for i in range(2, No * 2 + 1, 2):
+        print(i)
+
+
+def DisplayOdd(No):
+    print(threading.current_thread().name)
+    for i in range(1, No * 2, 2):
+        print(i)
+
+def main():
+
+    even = threading.Thread(name="Even", target=DisplayEven, args=(10,))
+    odd = threading.Thread(name="Odd", target=DisplayOdd, args=(10,))
+
+    even.start()
+    even.join()
+
+    odd.start()
+    odd.join()
+
+if __name__ == "__main__":
+    main()
