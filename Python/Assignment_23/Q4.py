cbc7da6db8e881e888bc53ff66d09ea950276196
SurajGardi <surajgardi0707@gmail.com>
2026-08-25 14:50:34 +0530

    Assignment_23 Q_04 added

---
+++ Python/Assignment_23/Q4.py
@@ -0,0 +1,50 @@
+"""
+4: Write a program that counts how many odd numbers exist between
+1 and N.
+Input
+Data = [1000000, 2000000, 3000000, 4000000]
+Expected Output Format
+Process ID : 1237
+Input Number : 1000000
+Odd Number Count : 500000
+
+"""
+import multiprocessing
+import os
+
+def CountOdd(No):
+
+    PID = os.getpid()
+
+    Count = 0
+
+    # for i in range(1, No + 1):
+    #     if i % 2 != 0:
+    #         Count = Count + 1
+
+    for i in range(1, No + 1, 2):
+        Count = Count + 1
+
+    return PID, No, Count
+
+
+def main():
+    Data = [1000000,2000000,3000000,4000000]
+
+    Result = []
+
+    pobj = multiprocessing.Pool()
+
+    Result = pobj.map(CountOdd, Data)
+
+    pobj.close()
+    pobj.join()
+
+    for PID, No, Count in Result:
+        print("Process ID : ",PID)
+        print("Input Number : ",No)
+        print("Count of Odd Numbers : ",Count)  
+        print("---------------------------")      
+
+if __name__ == "__main__":
+    main()
