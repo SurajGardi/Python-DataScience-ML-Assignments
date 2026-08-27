aadb881625e20e2d53f6f04824b77118cd987369
SurajGardi <surajgardi0707@gmail.com>
2026-08-27 11:48:06 +0530

    FH Assignment_29 Q2 added

---
+++ File Handling+Scheduling/Python FH Assignment 29/Q2.py
@@ -0,0 +1,34 @@
+# Q2) Display File Contents
+# Problem Statement:
+# Write a program which accepts a file name from the user, opens that file, and displays the entire contents on the
+# console.
+# Input:
+# Demo.txt
+# Expected Output:
+# Display contents of Demo.txt on console.
+
+
+import os
+
+def main():
+    file = input("Enter File Name : ")
+
+    for FolderName, SubFolder, FileName in os.walk("Marvellous"):
+        if file in FileName:
+
+            path = os.path.join(FolderName, file)
+
+            fobj = open(path, "r")
+
+            Data = fobj.read()
+
+            print(Data)
+
+            fobj.close()
+
+        print("File is not present in current directory")
+
+
+
+if __name__ == "__main__":
+    main()
