30867f4172da4cf0b739a05be5daac6e4208c964
SurajGardi <surajgardi0707@gmail.com>
2026-08-28 16:02:37 +0530

    FH+Scheduling Assignment_30 Q2 added

---
+++ File Handling+Scheduling/Python Scheduling Assignment 30/Q2.py
@@ -0,0 +1,33 @@
+"""
+
+2: Write a Python program that displays the current date and time
+after every one minute.
+Use the datetime module.
+Expected output:
+Current Date and Time: 25-07-2026 04:30:00 PM
+
+"""
+
+import schedule
+import time
+import datetime
+
+def Display():
+
+    CurrentTime = datetime.datetime.now()
+
+    print("Current Date and Time :",
+          CurrentTime.strftime("%d-%m-%Y %I:%M:%S %p"))
+
+
+def main():
+
+    schedule.every(1).minutes.do(Display)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
