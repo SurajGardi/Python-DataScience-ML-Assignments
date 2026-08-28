ef6cb9db90e7ccb6374d75932ec53be4284e86bd
SurajGardi <surajgardi0707@gmail.com>
2026-08-28 16:13:34 +0530

    FH+Scheduling Assignment_31 Q2 added

---
+++ File Handling+Scheduling/Python FH+Scheduling Assignment 31/Q2.py
@@ -0,0 +1,32 @@
+"""
+
+2: Create a function named:
+DisplayMessage(message)
+Schedule the function using:
+schedule.every(5).seconds.do(DisplayMessage, message)
+The message should be accepted from the user.
+
+"""
+
+import schedule
+import time
+
+def DisplayMessage(message):
+    print(message)
+
+
+def main():
+
+    message = input("Enter Message : ")
+
+    schedule.every(5).seconds.do(DisplayMessage, message)
+
+    while True:
+
+        schedule.run_pending()
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
