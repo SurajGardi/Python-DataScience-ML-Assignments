f1da9e270811cf80301bef8cd676375ac3b8eea7
SurajGardi <surajgardi0707@gmail.com>
2026-08-28 16:06:00 +0530

    FH+Scheduling Assignment_30 Q6 added

---
+++ File Handling+Scheduling/Python Scheduling Assignment 30/Q6.py
@@ -0,0 +1,31 @@
+"""
+
+6: Write a script that schedules the following tasks:
+• Print Lunch Time! every day at 1:00 PM.
+• Print Wrap up work every day at 6:00 PM.
+Both tasks should be handled by separate functions.
+
+"""
+
+
+import schedule
+import time
+
+def LunchTime():
+    print("It's Lunch Time")
+
+def WrapUp():
+    print("Heyyy Wrap Up Work - Jitna pagar utni dihadi..")
+
+def main():
+
+    schedule.every().day.at("13:00").do(LunchTime)
+    schedule.every().day.at("18:00").do(WrapUp)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
